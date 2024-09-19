4db63d5439955f196c87e5fa73890738f2248ba7
Your Name <k.bristi057@gmail.com>
2024-09-19 21:28:35 +0600

    added the add money form

---
+++ js/home.js
@@ -0,0 +1,20 @@
+document.getElementById('add-money').addEventListener('click', function(event){
+event.preventDefault();
+
+const accountNum=document.getElementById('account-num').value;
+
+const addMoney=document.getElementById('add-money-amount').value;
+const availableAmount=parseFloat(addMoney);
+const addPin=document.getElementById('add-money-pin').value;
+if(addPin==='1234' && accountNum==='12345678'){
+    const presentAmount=document.getElementById('present-amount');
+   const amount= parseFloat(presentAmount.innerText);
+    const newBalance =(amount + availableAmount);
+    console.log(newBalance);
+document.getElementById('present-amount').innerText=newBalance;
+
+}
+else{
+    alert ('Invalid Information');
+}
+})
