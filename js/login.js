998b02577f13b65b6b14cf4bf5b1430c53ae3dbe
Your Name <k.bristi057@gmail.com>
2024-09-19 18:15:58 +0600

    added the login page

--- js/login.js
+++ js/login.js
@@ -4,6 +4,14 @@ event.preventDefault();
     // console.log("login button clicked");
 
 const phoneNum=document.getElementById('phone-num').value;
-console.log(phoneNum);
+const pinNum=document.getElementById('input-pin').value;
+console.log(phoneNum, pinNum);
+if(phoneNum==='0' && pinNum==='1234'){
+    console.log("You're Logged in");
+    window.location.href= './home.html';
+}
+else{
+    alert ('Invalid Data');
+}
 
 })
