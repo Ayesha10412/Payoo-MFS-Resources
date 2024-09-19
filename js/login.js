646f913f358a445f3ec622f53dc18dac19196af1
Your Name <k.bristi057@gmail.com>
2024-09-19 17:42:39 +0600

    added the new files

---
+++ js/login.js
@@ -0,0 +1,9 @@
+document.getElementById('btn-login').addEventListener('click', function(event){
+
+event.preventDefault();
+    // console.log("login button clicked");
+
+const phoneNum=document.getElementById('phone-num').value;
+console.log(phoneNum);
+
+})
